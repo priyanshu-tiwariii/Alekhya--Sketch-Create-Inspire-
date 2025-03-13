b2dc914d035035c42f88d1fd2028ee1190e8357b
impriyanshu29 <iampriyanshu2901@outlook.com>
2025-03-14 02:12:46 +0530

    fix(pacakge_manager) - migrated npm to pnpm 😊😊

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,13 +1,11 @@
-// tailwind config is required for editor support
-
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx",
-    "./components/**/*.tsx",
-    "../../packages/ui/**/*.{js,ts,jsx,tsx}"
-    
+  content: [
+    "./app/**/*.tsx", // Scan files in the app directory
+    "./components/**/*.tsx", // Scan files in the components directory
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}", // Explicitly point to the source files in your UI package
   ],
   presets: [sharedConfig],
 };
