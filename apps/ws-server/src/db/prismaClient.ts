bdf717255109b061d9c0b08739426cb0b33014c8
impriyanshu29 <iampriyanshu2901@outlook.com>
2025-03-26 21:59:44 +0530

    feat (singelton) -> added prisma singelton in http and ws server

--- apps/ws-server/src/db/prismaClient.ts
+++ apps/ws-server/src/db/prismaClient.ts
@@ -1,4 +1,20 @@
-import { PrismaClient } from "@repo/db/prismaClient";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// eslint-disable-next-line
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
