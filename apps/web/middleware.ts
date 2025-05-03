cdd7d55d638aa2fc7f8aa05c0563922150b7f59b
impriyanshu29 <iampriyanshu2901@outlook.com>
2025-05-03 14:34:53 +0530

    refactor (middleware) ->  update auth check for collaboration routes

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -8,5 +8,5 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/profile/:path*"], 
+  matcher: ["/dashboard/:path*", "/profile/:path*","/canvas/:path*"],
 };
